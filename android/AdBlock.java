afbf572845bc2e6326aeff80b8fedc6081736af4
Gregor Santner <gsantner@mailbox.org>
2017-10-31 18:15:07 +0100

    Update domain

--- android/AdBlock.java
+++ android/AdBlock.java
@@ -1,6 +1,6 @@
 /*
  * ---------------------------------------------------------------------------- *
- * Gregor Santner <gsantner.github.io> wrote this file. You can do whatever
+ * Gregor Santner <gsantner.net> wrote this file. You can do whatever
  * you want with this stuff. If we meet some day, and you think this stuff is
  * worth it, you can buy me a coke in return. Provided as is without any kind
  * of warranty. No attribution required.                  - Gregor Santner
