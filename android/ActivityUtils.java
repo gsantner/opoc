5461af5febb52e2dd5b0b405181de013c8daf98f
Gregor Santner <gsantner@mailbox.org>
2017-12-04 13:44:33 +0100

    Update opoc

--- android/ActivityUtils.java
+++ android/ActivityUtils.java
@@ -12,8 +12,11 @@
 package net.gsantner.opoc.util;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
@@ -25,6 +28,7 @@ import android.util.TypedValue;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
 
 
 @SuppressWarnings({"WeakerAccess", "unused", "SameParameterValue", "SpellCheckingInspection"})
@@ -96,10 +100,10 @@ public class ActivityUtils extends net.gsantner.opoc.util.ContextUtils {
         }
     }
 
-    public void showSoftKeyboard(){
+    public void showSoftKeyboard() {
         InputMethodManager inputMethodManager = (InputMethodManager) _activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         if (_activity.getCurrentFocus() != null && _activity.getCurrentFocus().getWindowToken() != null) {
-            inputMethodManager.showSoftInput(_activity.getCurrentFocus() ,InputMethodManager.SHOW_FORCED);
+            inputMethodManager.showSoftInput(_activity.getCurrentFocus(), InputMethodManager.SHOW_FORCED);
         }
     }
 
@@ -123,6 +127,16 @@ public class ActivityUtils extends net.gsantner.opoc.util.ContextUtils {
         dialog.show();
     }
 
+    public void showDialogWithRawFileInWebView(String fileInRaw, @StringRes int resTitleId) {
+        WebView wv = new WebView(_context);
+        wv.loadUrl("file:///android_res/raw/" + fileInRaw);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(_context)
+                .setPositiveButton(android.R.string.ok, null)
+                .setTitle(resTitleId)
+                .setView(wv);
+        dialog.show();
+    }
+
     // Toggle with no param, else set visibility according to first bool
     public void toggleStatusbarVisibility(boolean... optionalForceVisible) {
         WindowManager.LayoutParams attrs = _activity.getWindow().getAttributes();
@@ -135,5 +149,19 @@ public class ActivityUtils extends net.gsantner.opoc.util.ContextUtils {
             attrs.flags |= flag;
         }
         _activity.getWindow().setAttributes(attrs);
+    }
+
+    public void showRateOnGplayDialog() {
+        String pkgId = "details?id=" + _activity.getPackageName();
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, Uri.parse("market://" + pkgId));
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                (Build.VERSION.SDK_INT >= 21 ? Intent.FLAG_ACTIVITY_NEW_DOCUMENT : Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET) |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        try {
+            _activity.startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            _activity.startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/" + pkgId)));
+        }
     }
 }
